2b3c749d90343d8eff9657fd5667bf50e07fcf2b
ubyoth <ubyakpan@gmail.com>
2026-02-18 15:42:15 +0100

    Create 2026-02-18-new-legal-brief.tsx
    Add first Knowledge Centre post

---
+++ content/knowledge/2026-02-18-new-legal-brief.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NewLegalBrief: React.FC = () => (
+  <div>
+    <p>This is my new legal brief about [topic].</p>
+    <p>Write your content here for the Knowledge Centre.</p>
+  </div>
+);
+
+export default NewLegalBrief;
