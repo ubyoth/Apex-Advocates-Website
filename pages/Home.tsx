04d19ebea375c7ef76e86a4c85d037b51cb7ceb3
ubyoth <ubyakpan@gmail.com>
2026-02-19 16:34:48 +0100

    Update Home.tsx

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -194,7 +194,7 @@ const Home: React.FC = () => {
 
           <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-12 items-center">
             {[
-              "https://res.cloudinary.com/dq7vjblwd/image/upload/f_auto,q_auto/v1771515001/teambonding_hgikfy.webp",
+              "https://res.cloudinary.com/dq7vjblwd/image/upload/v1771515238/teambondingng_hsramv.png",
               "/clients/client2.png",
               "/clients/client3.png",
               "/clients/client4.png",
